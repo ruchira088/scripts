37f43d0bb63cb7920d9ba8d4ac226e674d0927ce
Ruchira (Richie) Jayasekara <ruchira088@gmail.com>
2017-05-20 13:59:26 +1000

    Initial commit

---
+++ ubuntu_init.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+sudo apt-get install curl -y
+
+# NodeJS
+curl -sL https://deb.nodesource.com/setup_7.x | sudo -E bash -
+sudo apt-get install -y nodejs
+
+sudo apt-get install -y build-essential
+
+# Java
+sudo add-apt-repository ppa:webupd8team/java
+sudo apt-get update
+
+sudo apt-get install oracle-java8-installer -y
+
+# Docker
+sudo apt-get install apt-transport-https ca-certificates software-properties-common -y
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+
+sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
+sudo apt-get update
+
+sudo apt-get install docker-ce -y
+sudo groupadd docker
+sudo usermod -aG docker $USER
+sudo systemctl enable docker
+
+# Docker Compose
+sudo wget -O /usr/local/bin/docker-compose https://github.com/docker/compose/releases/download/1.13.0/docker-compose-`uname -s`-`uname -m`
+sudo chmod +x /usr/local/bin/docker-compose
+
+# SBT
+echo "deb https://dl.bintray.com/sbt/debian /" | sudo tee -a /etc/apt/sources.list.d/sbt.list
+sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 2EE0EA64E40A89B84B2DF73499E82A75642AC823
+sudo apt-get update
+sudo apt-get install sbt -y
+
