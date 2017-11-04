4d4fb3738a572a5a2382d47cb1851cb7013d7d00
Ruchira (Richie) Jayasekara <ruchira088@gmail.com>
2017-11-04 20:18:45 +1100

    Updating Docker Compose

--- ubuntu_init.sh
+++ ubuntu_init.sh
@@ -17,7 +17,7 @@ sudo apt-get install -y build-essential
 # sudo apt-get install oracle-java8-installer -y
 
 # Java (OpenJDK)
-sudo apt-get install openjdk-8-jdk -y
+sudo apt-get install bc openjdk-8-jdk -y
 
 # Docker
 sudo apt-get install apt-transport-https ca-certificates software-properties-common -y
@@ -32,7 +32,7 @@ sudo usermod -aG docker $USER
 sudo systemctl enable docker
 
 # Docker Compose
-sudo curl -L https://github.com/docker/compose/releases/download/1.16.1/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose
+sudo curl -L https://github.com/docker/compose/releases/download/1.17.0/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
 
 # SBT
