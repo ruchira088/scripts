f51a4b1e1303dd52da13d371db9ef0d70a099b4c
Ruchira (Richie) Jayasekara <ruchira088@gmail.com>
2018-03-11 17:54:13 +1100

    Installing Google Chrome

--- desktop_init.sh
+++ desktop_init.sh
@@ -8,6 +8,8 @@ sudo apt-get install sublime-text
 # Google Chrome
 wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | sudo apt-key add -
 echo 'deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable main' | sudo tee /etc/apt/sources.list.d/google-chrome.list
+sudo apt-get update
+sudo apt-get install google-chrome-stable -y
 
 # AWS CLI
 sudo apt-get install awscli -y
