7003d11711dab726c685ed29ea302511d356673d
Ruchira (Richie) Jayasekara <ruchira088@gmail.com>
2018-04-30 20:36:34 +1000

    Updating the docker-compose version

--- ubuntu_init.sh
+++ ubuntu_init.sh
@@ -37,7 +37,7 @@ sudo usermod -aG docker $USER
 sudo systemctl enable docker
 
 # Docker Compose
-sudo curl -L https://github.com/docker/compose/releases/download/1.17.0/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose
+sudo curl -L https://github.com/docker/compose/releases/download/1.21.0/docker-compose-$(uname -s)-$(uname -m) -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
 
 # SBT
