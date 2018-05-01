ace4315727db1c0c23267a57ee30067c0cafb243
Ruchira (Richie) Jayasekara <ruchira088@gmail.com>
2018-05-01 21:20:15 +1000

    Updating the NodeJS version

--- desktop_init.sh
+++ desktop_init.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+
 # Sublime
 sudo apt-get install apt-transport-https wget -y
 wget -qO - https://download.sublimetext.com/sublimehq-pub.gpg | sudo apt-key add -
@@ -10,9 +12,6 @@ wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | sudo apt-ke
 echo 'deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable main' | sudo tee /etc/apt/sources.list.d/google-chrome.list
 sudo apt-get update
 sudo apt-get install google-chrome-stable -y
-
-# AWS CLI
-sudo apt-get install awscli -y
 
 # kubectl
 sudo apt install snapd -y
--- ubuntu_init.sh
+++ ubuntu_init.sh
@@ -4,7 +4,7 @@ sudo apt-get update
 sudo apt-get install curl -y
 
 # NodeJS
-curl -sL https://deb.nodesource.com/setup_9.x | sudo -E bash -
+curl -sL https://deb.nodesource.com/setup_10.x | sudo -E bash -
 sudo apt-get install -y nodejs
 
 sudo apt-get install -y build-essential
