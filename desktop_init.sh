be1f16588fa9293d88816d4235bb933df9212cd7
Ruchira (Richie) Jayasekara <ruchira088@gmail.com>
2018-05-12 11:16:43 +1000

    Updating the VirtualBox repo

--- desktop_init.sh
+++ desktop_init.sh
@@ -14,7 +14,7 @@ sudo apt-get update
 sudo apt-get install google-chrome-stable -y
 
 # VirtualBox
-echo "deb https://download.virtualbox.org/virtualbox/debian `lsb_release --codename | cut -f2` contrib" | sudo tee /etc/apt/sources.list.d/virtualbox.list
+echo "deb [arch=amd64] https://download.virtualbox.org/virtualbox/debian `lsb_release --codename | cut -f2` contrib" | sudo tee /etc/apt/sources.list.d/virtualbox.list
 wget -q https://www.virtualbox.org/download/oracle_vbox_2016.asc -O- | sudo apt-key add -
 wget -q https://www.virtualbox.org/download/oracle_vbox.asc -O- | sudo apt-key add -
 sudo apt-get update
