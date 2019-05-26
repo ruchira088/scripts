dcbb85157335037348fae1715424b16dcb5b5e63
Ruchira (Richie) Jayasekara <ruchira088@gmail.com>
2019-05-27 09:15:24 +1000

    Updating the Terraform version

--- ubuntu_init.sh
+++ ubuntu_init.sh
@@ -52,7 +52,7 @@ sudo ln -sf $HOME/.local/bin/aws /usr/local/bin
 
 # Terraform
 sudo apt install unzip -y && \
-curl -o terraform.zip https://releases.hashicorp.com/terraform/0.11.13/terraform_0.11.13_linux_amd64.zip && \
+curl -o terraform.zip https://releases.hashicorp.com/terraform/0.12.0/terraform_0.12.0_linux_amd64.zip && \
 unzip terraform.zip && rm terraform.zip && chmod +x terraform && \
 sudo mv -f $PWD/terraform /usr/local/bin/
 
