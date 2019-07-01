ad45e55e00552574cce380f035f0e40f0d83a45f
Ruchira (Richie) Jayasekara <ruchira088@gmail.com>
2019-07-01 20:48:53 +1000

    Breaking up the Docker permission addition command

--- ubuntu_init.sh
+++ ubuntu_init.sh
@@ -33,9 +33,9 @@ curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add - && 
 sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" && \
 sudo apt update && \
 
-sudo apt install docker-ce -y && \
-sudo groupadd docker && \
-sudo usermod -aG docker $USER && \
+sudo apt install docker-ce -y
+sudo groupadd docker
+sudo usermod -aG docker $USER
 sudo systemctl enable docker
 
 # Docker Compose
